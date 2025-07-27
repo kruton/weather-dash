b9cb53e279f5c9f61327fd74b5e34813c4e49e76
Kenny Root <kenny@the-b.org>
2025-07-27 11:02:39 -0700

    Fix the client file names
    Files compiled with mpy-cross need to have the extension .mpy instead of
    .py or else Micropython won't be able to understand them. Also we don't
    need an API_KEY anymore and will just pass the NAME to be rendered as
    the place name directly.

--- client/weather.py
+++ client/weather.py
@@ -14,9 +14,9 @@ We need somewhere to save the jpg for display.
 
 # Configure these items to match.
 
-API_KEY = "abc123"  # OpenWeathMap API key
+NAME = "San Francisco, California"  # Location name
 LAT = "37.7749"  # Latitude for weather
-LONG = "122.4194"  # Longitude for weather
+LONG = "-122.4194"  # Longitude for weather
 UPDATE_INTERVAL = 240  # Number of minutes between updates
 
 
@@ -28,9 +28,14 @@ HEIGHT = None
 gc.collect()  # We're really gonna need that RAM!
 
 FILENAME = "/sd/weather.png"
-ENDPOINT = f"https://weather-dash.their.net/api/screenshot?lat={LAT}&long={LONG}&api_key={API_KEY}&width={WIDTH}&height={HEIGHT}"
+ENDPOINT = f"https://weather-dash.their.net/api/screenshot?lat={LAT}&long={LONG}&name={NAME}&width={WIDTH}&height={HEIGHT}"
 
-sd_spi = machine.SPI(0, sck=machine.Pin(18, machine.Pin.OUT), mosi=machine.Pin(19, machine.Pin.OUT), miso=machine.Pin(16, machine.Pin.OUT))
+sd_spi = machine.SPI(
+    0,
+    sck=machine.Pin(18, machine.Pin.OUT),
+    mosi=machine.Pin(19, machine.Pin.OUT),
+    miso=machine.Pin(16, machine.Pin.OUT),
+)
 sd = sdcard.SDCard(sd_spi, machine.Pin(22))
 uos.mount(sd, "/sd")
 gc.collect()  # Claw back some RAM!
@@ -70,7 +75,8 @@ def draw():
         graphics.rectangle(0, (HEIGHT // 2) - 20, WIDTH, 40)
         graphics.set_pen(1)
         graphics.text("Unable to display image!", 5, (HEIGHT // 2) - 15, WIDTH, 2)
-        graphics.text("Check your network settings in secrets.py", 5, (HEIGHT // 2) + 2, WIDTH, 2)
+        graphics.text(
+            "Check your network settings in secrets.py", 5, (HEIGHT // 2) + 2, WIDTH, 2
+        )
 
     graphics.update()
-
